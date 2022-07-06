49dc9a1853ae22cb8cea241a6bd76481d319fe9b
RossM <RossM@users.noreply.github.com>
2022-07-06 14:33:38 -0700

    embiggen

--- models/networks.py
+++ models/networks.py
@@ -199,7 +199,7 @@ def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal'
     elif netD == 'n_layers':  # more options
         net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer)
     elif netD == 'deep':  # more options
-        net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer, n_layers2=3)
+        net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer, n_layers2=12)
     elif netD == 'pixel':     # classify if each pixel is real or fake
         net = PixelDiscriminator(input_nc, ndf, norm_layer=norm_layer)
     else:
@@ -473,7 +473,7 @@ class UnetGenerator(nn.Module):
                                                  submodule=unet_block, outermost=True, norm_layer=norm_layer,
                                                  last=last)  # add the outermost layer
             model += [unet_block]
-        model += [Painter2d(ngf, ngf, 1), nn.Tanh()]
+        model += [Painter2d(ngf, ngf * 2, 1), nn.Tanh()]
         self.model = nn.Sequential(*model)
             
     def forward(self, input):
